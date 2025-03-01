7281c0125047218724c3537e275330c6e015a71b
Sanjeet Kumar <156220916+SanjeetKumar2003@users.noreply.github.com>
2025-03-01 23:50:31 +0530

    completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,11 +1,11 @@
 import os
 
 AWS_S3_BUCKET_NAME="wafer-fault"
-MONGO_DATABASE_NAME="pwskills"
-MONGO_COLLECTION_NAME="waferfault"
+MONGO_DATABASE_NAME="Sensor_project"
+MONGO_COLLECTION_NAME="wafer_fault"
 
 TARGET_COLUMN="quality"
-MONGO_DB_URL="mongodb+srv://snshrivas@cluster0.u4c4.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL="mongodb+srv://skssanjeet9835:0GInb5glPzvuGPMw@cluster0.g3cma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 MODEL_FILE_NAME="model1"
 MODEL_FILE_EXTENSION=".pkl"
